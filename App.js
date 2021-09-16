4b0c016194c290037036e4738c7036ce2c330820
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2021-09-16 13:21:39 -0300

    Finalizado de forma básica

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [reseted, setReseted] = useState(false);
+  const [textMain, setText] = useState("É só clicar amigo!");
+  const [srcImage, setSrc] = useState("");
+
+  function reset(){
+      // if(count == 17) easterEggActivated = true;
+      setCount(0);
+      setReseted(true);
+  }
+
+  useEffect(() => {
+     var waitEasterEgg;
+     if(count == 17){
+         // document.title = "Quer um cookie?";
+         setText("Quer um cookie?");
+         waitEasterEgg = setTimeout(() => {
+             setText("Ovo de pascoa!!!!")
+             setSrc(require("./assets/download.jpeg"));
+         }, 10000);
+     }
+     else if(count == 0){
+         setText("É só clicar");
+         try{clearTimeout(waitEasterEgg);}
+         catch{}
+     }
+     else{
+         // document.title = `Foram ${count} clicks!`;
+         setText(`Foram ${count} clicks!`);
+         try{clearTimeout(waitEasterEgg);}
+         catch{}
+     }
+  }, [count]);
+
+  useEffect(() =>{
+      document.title = "Resetado";
+      setTimeout(() => {document.title = "Contador de clicks";}, 3000);
+  }, [reseted]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to the click counter!</Text>
+      <Text>{textMain}</Text>
+      <Button style={styles.btn} onPress={() => setCount(prevCount => prevCount + 1)} title="Click!"/>
+      <br/>
+      <Button onPress={() => reset()} title="reset count"/>
+      <br/>
+      <Image source={srcImage} style={styles.easterEgg} resizeMode="contain"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +62,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  easterEgg: {
+      flex: 1,
+      width: 500,
+      height: 500
+  },
+  btn: {
+      flex: 1,
+      marginTop: 20
+  }
 });
